f0af79a75b547cfb9802fe93b9021693c670e676
Aman Kumar Pandey <160414387+amanpandey7647@users.noreply.github.com>
2024-07-28 16:02:30 +0530

    Adding Python3 pip
    Adding python3-pip3 installation module and some minor fixes

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt install wget -y
 sudo apt install python -y
+sudo apt install python3-pip -y
 sudo apt install git -y
 sudo apt install python3-venv -y
