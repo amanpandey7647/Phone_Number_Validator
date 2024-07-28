2b6bbc769f98755c13e18f8acc5321ef77ac90de
Aman Kumar Pandey <160414387+amanpandey7647@users.noreply.github.com>
2024-07-28 15:57:47 +0530

    Add Sudo
    #Sudo command req to install on root dir

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
-apt install wget -y
-apt install python -y
+sudo apt install wget -y
+sudo apt install python -y
 sudo apt install git -y
 sudo apt install python3-venv -y
